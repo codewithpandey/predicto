9ab3f14740bcf3588b76a3d9eda682ca9da77e51
Mithu Mondal <mithu2649@gmail.com>
2021-11-29 00:15:09 +0530

    Moved google prank to 'google_prank.txt' for reference

--- client/content.js
+++ client/content.js
@@ -27,25 +27,6 @@ window.addEventListener('DOMFocusIn', function () {
         stars[digit].click();
     }
 
-
-    // for fun
-    // let body = document.querySelector('body');
-    // let jokes = [
-    //     "Sorry, Google is down today!",
-    //     "<a href='https://duckduckgo.com'>DuckDuckGo</a> is better.",
-    //     "I'm Watching You... 😶",
-    //     "I know you better than you know yourself. I'm google bro.",
-    //     "I'm Sick. I've overdosed on privacy",
-    //     "<a href='https://duckduckgo.com'>DuckDuckGo</a> is better.",
-    //     "I've been admitted in a hospital. I've overdosed on monopoly.",
-    //     "<span style='color:red'>Google Virus found!!</span>",
-    //     "I'm tired.",
-    //     "😴"
-    // ]
-
-    // body.innerHTML = `<p>${ jokes[Math.floor(Math.random() * jokes.length)] }</p>`;
-
-
 }, false);
 
 
