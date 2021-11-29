c2c6501d05d629a821343e5147c3821084631514
Mithu Mondal <mithu2649@gmail.com>
2021-11-29 23:27:48 +0530

    minor code refactoring

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 
-<div style="text-align:center">
+<div>
 
-<img src="https://github.com/mithu2649/predicto/raw/main/client/resources/images/logo128.png"/>
+<img src="https://github.com/mithu2649/predicto/raw/main/client/resources/images/logo48.png"/>
+<img src="https://github.com/mithu2649/predicto/raw/main/client/resources/images/logo48.png"/>
+<img src="https://github.com/mithu2649/predicto/raw/main/client/resources/images/logo48-half.png"/>
+<img src="https://github.com/mithu2649/predicto/raw/main/client/resources/images/logo48-empty.png"/>
 
 # Predicto - Review Rate Predictor<br>
 _A chrome extension to predict star ratings according to the customer's review._
@@ -20,6 +23,6 @@ _A chrome extension to predict star ratings according to the customer's review._
   - load unpacked
   - select the 'client' folder from the cloned repo
   - once installed, head over to flipkart's 'write a review page'
-  - Type in your review, and make sure to include any of these keywords in the review - awesome  worst, bad, good, decent, great etc.
+  - Type in your review, and make sure to include any of these keywords in the review - awesome,  worst, bad, good, decent, great etc.
 
 ### Happy Coding!
--- client/content.js
+++ client/content.js
@@ -1,39 +1,43 @@
-chrome.runtime.sendMessage({ todo: "showPageAction" })
+chrome.runtime.sendMessage({ todo: "showPageAction" });
 
 window.addEventListener('DOMFocusIn', function () {
 
-    let stars = [];
-    let review = document.querySelector('textarea._3kRe7w');
+    console.clear();
 
-    document.querySelector('._2teBVu').childNodes.forEach(child => stars.push(child));
+    let reviewContainer = document.querySelector('textarea._3kRe7w');
+    let stars = [];
+
+    document.querySelector('._2teBVu')
+            .childNodes
+            .forEach(child => stars.push(child));
 
     stars.reverse();
     stars.unshift(0);
 
-    review.addEventListener('keyup', function (e) {
+    reviewContainer.addEventListener('keyup', function (e) {
 
-        let reviewText = e.target.value;
+        let review = e.target.value;
 
-        if (reviewTextIncludes('awesome'))  updateRating(5);
-        if (reviewTextIncludes('amazing'))  updateRating(5);
-        if (reviewTextIncludes('decent'))   updateRating(3);
-        if (reviewTextIncludes('worst'))    updateRating(1);
-        if (reviewTextIncludes('great'))    updateRating(4);
-        if (reviewTextIncludes('cool'))     updateRating(3);
-        if (reviewTextIncludes('nice'))     updateRating(4);
-        if (reviewTextIncludes('bad'))      updateRating(1);
+        if (review.includes('awesome')) updateRating(5);
+        if (review.includes('amazing')) updateRating(5);
+        if (review.includes('decent'))  updateRating(3);
+        if (review.includes('great'))   updateRating(4);
+        if (review.includes('worst'))   updateRating(1);
+        if (review.includes('cool'))    updateRating(3);
+        if (review.includes('nice'))    updateRating(4);
+        if (review.includes('bad'))     updateRating(1);
 
-        function reviewTextIncludes(word) {
-            //splits the review text at the spaces, and checks if the given word exists.
-            return reviewText.split(' ').includes(word) ? true : false;
-        }
     });
+
+    String.prototype.includes = function (keyword) {
+        return this.split(' ').includes(keyword) ? true : false;
+    }
 
     function updateRating(digit) {
         //early returns, fail checks.
-        if(digit == undefined || digit == null) return;
-        if(digit < 0 || digit > 5)   return console.error('rating not within the range 1 to 5');
-        if(!Number.isInteger(digit)) return console.error(`rating must be an Integer, recieved ${typeof digit}!`)
+        if (digit == undefined || digit == null) return;
+        if (digit < 1 || digit > 5)   return console.error('rating not within the range 1 to 5');
+        if (!Number.isInteger(digit)) return console.error(`rating must be an Integer, recieved ${typeof digit}!`)
 
         stars[digit].click();
     }
